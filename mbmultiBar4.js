be2d7e9b20761c929a13d173b84d0bdb613da9bf
mbaldassaro <mbaldassaro@gmail.com>
2014-06-02 12:18:41 -0400

    added multibar probs facing bangladesh

---
+++ mbmultiBar4.js
@@ -0,0 +1,32 @@
+d3.json("mbmultiBar4.json", function(error, data) {
+     
+var chart;
+    
+nv.addGraph(function() {
+  chart = nv.models.multiBarChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value / 100 })
+      .margin({top: 30, right: 20, bottom: 50, left: 75})
+      //.showValues(false)
+      //.tooltips(true)
+      //.barColor(d3.scale.category10().range())
+      .transitionDuration(500)
+      //.stacked(true)
+      //.showControls(false);
+
+  chart.yAxis 
+    .tickFormat(d3.format('%'));
+  //chart.xAxis
+    //.tickFormat(d3.format('d'));
+
+  d3.select('#chart1 svg')
+      .datum(data)
+      .call(chart);
+
+  nv.utils.windowResize(chart.update);
+
+  chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+  return chart;
+});
+})
